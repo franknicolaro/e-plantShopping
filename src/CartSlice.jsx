44c15100c0db7d460fcdac92308dec4352b0cf31
Frank nicolaro <franknicolaro@gmail.com>
2025-03-04 15:00:07 -0500

    CartItems copmonent and UI updating

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -3,7 +3,8 @@ import { createSlice } from '@reduxjs/toolkit';
 export const CartSlice = createSlice({
   name: 'cart',
   initialState: {
-    items: [], // Initialize items as an empty array
+    items: [],  // Initialize items as an empty array
+    quantityInCart: 0, //Initialize a counter for number of items in cart(I couldnt figure out why items.length wasn't working).
   },
   reducers: {
     addItem: (state, action) => {
@@ -14,14 +15,24 @@ export const CartSlice = createSlice({
         } else{
             state.items.push({ name, image, cost, quantity: 1});
         }
+        state.quantityInCart++;
     },
     removeItem: (state, action) => {
-        state.items = state.items.filter( item => item.name !== action.payload);
+        console.log("removing...")
+        state.quantityInCart -= state.items.find(item => item.name === action.payload.name).quantity;
+        state.items = state.items.filter(item => item.name !== action.payload.name);
+        
     },
     updateQuantity: (state, action) => {
         const {name, quantity} = action.payload;
         const existingItem = state.items.find(item => item.name === name);
         if(existingItem){
+            if(existingItem.quantity < quantity){
+                state.quantityInCart++;
+            }
+            else{
+                state.quantityInCart--;
+            }
             existingItem.quantity = quantity;
         }
     },
